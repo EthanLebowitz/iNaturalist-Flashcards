a759e49f35223f293325555d277e5eaec2a375ea
Ethan Lebowitz <duckunreal@gmail.com>
2026-06-12 21:12:45 -0700

    Performance improvements

--- scripts/build-pages.mjs
+++ scripts/build-pages.mjs
@@ -101,11 +101,15 @@ ${GA_SNIPPET}
 </script>
 
 <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://api.inaturalist.org">
+<link rel="preconnect" href="https://inaturalist-open-data.s3.amazonaws.com" crossorigin>
+<link rel="dns-prefetch" href="https://static.inaturalist.org">
 <link rel="stylesheet" href="/app.css">
 <script
-			  src="https://code.jquery.com/jquery-3.5.1.js"
-			  integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc="
+			  src="https://code.jquery.com/jquery-3.5.1.min.js"
+			  integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
 			  crossorigin="anonymous">
 </script>
 <script>window.__DECK__ = '${deck.key}';</script>
